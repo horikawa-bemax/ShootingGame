02d4394aed63a7f93c9d814eade01075e915a415
masaaki horikawa <horikawa@bemax.jp>
2012-09-17 13:41:38 +0900

    弾ついか

---
+++ ShootingGame/src/bemax/shootinggame/Bullet.java
@@ -0,0 +1,19 @@
+package bemax.shootinggame;
+
+import android.graphics.Canvas;
+
+public class Bullet extends Sprite {
+
+	@Override
+	public void draw(Canvas canvas) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
